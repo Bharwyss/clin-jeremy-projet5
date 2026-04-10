3850219f0c4b1eb92ba4e54c38b065519747b54b
Jeremy <jeremy.clin78@gmail.com>
2026-04-10 10:20:10 +0200

    Initial commit

---
+++ src/test/java/com/openclassrooms/projet5/Projet5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.projet5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projet5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
